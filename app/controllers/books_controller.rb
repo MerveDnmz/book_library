abb9c3bc9b7338e6d9484aacae47270c06c34f20
merve donmez <mervedonmez99@gmail.com>
2016-08-19 18:57:28 +0300

    Add gem file for layout desing and last commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,13 @@ class BooksController < ApplicationController
   end
 
   def show
-     @reviews = Review.where(book_id: @book.id).order("created_at DESC")
+    @reviews = Review.where(book_id: @book.id).order("created_at DESC")
+
+    if @reviews.blank?
+    @avg_review = 0
+    else
+    @avg_review = @reviews.average(:rating).round(2)
+    end
   end
 
   def new
